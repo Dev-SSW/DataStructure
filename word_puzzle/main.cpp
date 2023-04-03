a52188533f9da214fdeaa04a911f3660c9d4c190
Dev-SSW <sgg919@naver.com>
2023-04-03 22:25:49 +0900

    puzzle update

--- word_puzzle/main.cpp
+++ word_puzzle/main.cpp
@@ -13,10 +13,8 @@ const string puzzle_file = "puzzle.txt";
 #include <vector>
 #include <ctime>
 set <string> s;
-vector <string> v;
+set <string> v;
 matrix <string> puzzle_grid(8, 8);
-
-
 
 void BookList_Read_Line(const string& bookname)
 {
@@ -54,162 +52,175 @@ void Word_puzzle::read_dic_file(const string& dic_file)
 
 void make_word_in_puzzle(matrix<string> puzzle_grid) {
 	
-	for (int row = 0; row < puzzle_grid.numrows(); row++) // 좌측 -> 우측 가로
-	{
-		string row_line = "";
-		for (int col = 0; col < puzzle_grid.numcols(); col++)
+	for (int a = 0; a < 6; a++) {
+		for (int row = 0; row < puzzle_grid.numrows(); row++) // 좌측 -> 우측 가로
 		{
-			row_line += puzzle_grid[row][col]; //한글자 씩 더해주기
-			row_line.erase(find_if(row_line.begin(), row_line.end(), ::isspace), row_line.end());
-
-			if (s.find(row_line) != s.end() && (row_line.length() >= 3))
+			string row_line = "";
+			for (int col = a; col < puzzle_grid.numcols(); col++)
 			{
-				v.push_back(row_line);
-			}
-		}
-	}
-	for (int row = puzzle_grid.numrows()-1; row >= 0; row--) // 우측 -> 좌측 가로
-	{
-		string row_line = "";
-		for (int col = puzzle_grid.numcols() - 1; col >= 0; col--)
-		{
-			row_line += puzzle_grid[row][col]; //한글자 씩 더해주기
-			row_line.erase(find_if(row_line.begin(), row_line.end(), ::isspace), row_line.end());
-
-			if (s.find(row_line) != s.end() && (row_line.length() >= 3))
-			{
-				v.push_back(row_line);
-			}
-		}
-	}
-	
-	for (int col = 0; col < puzzle_grid.numcols(); col++) // 위 -> 아래 세로
-	{
-		string col_line = "";
-		for (int row = 0; row < puzzle_grid.numrows(); row++)
-		{
-			col_line += puzzle_grid[row][col];
-			col_line.erase(find_if(col_line.begin(), col_line.end(), ::isspace), col_line.end());
-			if (s.find(col_line) != s.end() && (col_line.length() >= 3))
-			{
-				v.push_back(col_line);
-			}
-		}
-	}
-
-	for (int col = puzzle_grid.numcols() - 1; col >= 0; col--) // 아래 -> 위 세로
-	{
-		string col_line = "";
-		for (int row = puzzle_grid.numrows() - 1; row >= 0; row--)
-		{
-			col_line += puzzle_grid[row][col];
-			col_line.erase(find_if(col_line.begin(), col_line.end(), ::isspace), col_line.end());
-			if (s.find(col_line) != s.end() && (col_line.length() >= 3))
-			{
-				v.push_back(col_line);
-			}
-		}
-	}
-
-	for (int spin = 0; spin < 15; spin++) //대각선 15번 회전해야함 우측 상단에서 좌측 하단으로 가는 대각선이다.
-	{
-		string diagonal_line = "";
-		for (int i = 0; i < puzzle_grid.numcols(); i++)
-		{
-			int j = spin - i;
-			if (j >= 0 && j < 8) { //0보다 크거나 같고 8보다 작은 경우에
-				diagonal_line += puzzle_grid[i][j];
-				diagonal_line.erase(find_if(diagonal_line.begin(), diagonal_line.end(), ::isspace), diagonal_line.end());
-				if (s.find(diagonal_line) != s.end() && (diagonal_line.length() >= 3))
+				row_line += puzzle_grid[row][col]; //한글자 씩 더해주기
+				row_line.erase(find_if(row_line.begin(), row_line.end(), ::isspace), row_line.end());
+				if (s.find(row_line) != s.end() && (row_line.length() >= 3))
 				{
-					v.push_back(diagonal_line);
+					v.insert(row_line);
 				}
 			}
 		}
 	}
-
-	for (int spin = 0; spin < 15; spin++) //대각선 15번 회전해야함 좌측 하단에서 우측 상단으로 가는 대각선이다.
-	{
-		string diagonal_line = "";
-		for (int i = 0; i < puzzle_grid.numcols(); i++)
+	for (int a = 0; a < 6; a++) {
+		for (int row = puzzle_grid.numrows() - 1; row >= 0; row--) // 우측 -> 좌측 가로
 		{
-			int j = spin - i;
-			if (j >= 0 && j < 8) { //0보다 크거나 같고 8보다 작은 경우에
-				diagonal_line += puzzle_grid[j][i];
-				diagonal_line.erase(find_if(diagonal_line.begin(), diagonal_line.end(), ::isspace), diagonal_line.end());
-				if (s.find(diagonal_line) != s.end() && (diagonal_line.length() >= 3))
+			string row_line = "";
+			for (int col = puzzle_grid.numcols() - 1 - a; col >= 0; col--)
+			{
+				row_line += puzzle_grid[row][col]; //한글자 씩 더해주기
+				row_line.erase(find_if(row_line.begin(), row_line.end(), ::isspace), row_line.end());
+				if (s.find(row_line) != s.end() && (row_line.length() >= 3))
 				{
-					v.push_back(diagonal_line);
+					v.insert(row_line);
 				}
 			}
 		}
 	}
+	for (int a = 0; a < 6; a++) {
+		for (int col = 0; col < puzzle_grid.numcols(); col++) // 위 -> 아래 세로
+		{
+			string col_line = "";
+			for (int row = a; row < puzzle_grid.numrows(); row++)
+			{
+				col_line += puzzle_grid[row][col];
+				col_line.erase(find_if(col_line.begin(), col_line.end(), ::isspace), col_line.end());
+				if (s.find(col_line) != s.end() && (col_line.length() >= 3))
+				{
+					v.insert(col_line);
+				}
+			}
+		}
+	}
+	for (int a = 0; a < 6; a++) {
+		for (int col = puzzle_grid.numcols() - 1; col >= 0; col--) // 아래 -> 위 세로
+		{
+			string col_line = "";
+			for (int row = puzzle_grid.numrows() - 1 - a; row >= 0; row--)
+			{
+				col_line += puzzle_grid[row][col];
+				col_line.erase(find_if(col_line.begin(), col_line.end(), ::isspace), col_line.end());
+				if (s.find(col_line) != s.end() && (col_line.length() >= 3))
+				{
+					v.insert(col_line);
+				}
+			}
 
-	for (int SPIN = 0; SPIN <= 8; SPIN++) {    // 좌측 상단에서 우측 하단으로 오른쪽 위에서부터 8회전 까지
-		string diagonal_line = "";
-		for (int i = 0; i <= SPIN; i++) { 
-
-			if (SPIN <= 8) {
-				int j = puzzle_grid.numrows() - SPIN + i;               
-				if (j >= 0 && j < 8) {
+		}
+	}
+	for (int a = 0; a < 6; a++) {
+		for (int spin = 0; spin < 15; spin++) //대각선 15번 회전해야함 우측 상단에서 좌측 하단으로 가는 대각선이다.
+		{
+			string diagonal_line = "";
+			for (int i = a; i < puzzle_grid.numcols(); i++)
+			{
+				int j = spin - i;
+				if (j >= 0 && j < 8) { //0보다 크거나 같고 8보다 작은 경우에
 					diagonal_line += puzzle_grid[i][j];
 					diagonal_line.erase(find_if(diagonal_line.begin(), diagonal_line.end(), ::isspace), diagonal_line.end());
 					if (s.find(diagonal_line) != s.end() && (diagonal_line.length() >= 3))
 					{
-						v.push_back(diagonal_line);
-					}
-				}
-			} 
-		}
-	}
-	for (int SPIN = 0; SPIN < 8; SPIN++) {      //좌측 상단에서 우측 하단으로 왼쪽 밑에서부터 8회전까지
-		string diagonal_line = "";
-		for (int i = 0; i <= SPIN; i++) {
-
-			if (SPIN < 8) {
-				int j = puzzle_grid.numrows() - SPIN + i;
-				if (j >= 0 && j < 8) {
-					diagonal_line += puzzle_grid[j][i];
-					diagonal_line.erase(find_if(diagonal_line.begin(), diagonal_line.end(), ::isspace), diagonal_line.end());
-					if (s.find(diagonal_line) != s.end() && (diagonal_line.length() >= 3))
-					{
-						v.push_back(diagonal_line);
+						v.insert(diagonal_line);
 					}
 				}
 			}
 		}
 	}
-
-	for (int SPIN = 0; SPIN <= 8; SPIN++) {    // 좌측 상단에서 우측 하단으로 오른쪽 위에서부터 8회전 까지 (역방향)
-		string diagonal_line = "";
-		for (int i = SPIN; i >= 0; i--) {
-
-			if (SPIN <= 8) {
-				int j = puzzle_grid.numrows() - SPIN + i;
-				if (j >= 0 && j < 8) {
-					diagonal_line += puzzle_grid[i][j];
+	for (int a = 0; a < 6; a++) {
+		for (int spin = 0; spin < 15; spin++) //대각선 15번 회전해야함 좌측 하단에서 우측 상단으로 가는 대각선이다.
+		{
+			string diagonal_line = "";
+			for (int i = a; i < puzzle_grid.numcols(); i++)
+			{
+				int j = spin - i;
+				if (j >= 0 && j < 8) { //0보다 크거나 같고 8보다 작은 경우에
+					diagonal_line += puzzle_grid[j][i];
 					diagonal_line.erase(find_if(diagonal_line.begin(), diagonal_line.end(), ::isspace), diagonal_line.end());
 					if (s.find(diagonal_line) != s.end() && (diagonal_line.length() >= 3))
 					{
-						v.push_back(diagonal_line);
+						v.insert(diagonal_line);
 					}
 				}
 			}
 		}
 	}
+	for (int a = 0; a < 6; a++) {
+		for (int SPIN = 0; SPIN <= 8; SPIN++) {    // 좌측 상단에서 우측 하단으로 오른쪽 위에서부터 8회전 까지
+			string diagonal_line = "";
+			for (int i = a; i <= SPIN; i++) {
 
-	for (int SPIN = 0; SPIN < 8; SPIN++) {      //좌측 상단에서 우측 하단으로 왼쪽 밑에서부터 8회전까지 (역방향)
-		string diagonal_line = "";
-		for (int i = SPIN; i >= 0; i--) {
+				if (SPIN <= 8) {
+					int j = puzzle_grid.numrows() - SPIN + i;
+					if (j >= 0 && j < 8) {
+						diagonal_line += puzzle_grid[i][j];
+						diagonal_line.erase(find_if(diagonal_line.begin(), diagonal_line.end(), ::isspace), diagonal_line.end());
+						if (s.find(diagonal_line) != s.end() && (diagonal_line.length() >= 3))
+						{
+							v.insert(diagonal_line);
+						}
+					}
+				}
+			}
+		}
+	}
+	for(int a= 0; a<6; a++){
+		for (int SPIN = 0; SPIN < 8; SPIN++) {      //좌측 상단에서 우측 하단으로 왼쪽 밑에서부터 8회전까지
+			string diagonal_line = "";
+			for (int i = a; i <= SPIN; i++) {
 
-			if (SPIN < 8) {
-				int j = puzzle_grid.numrows() - SPIN + i;
-				if (j >= 0 && j < 8) {
-					diagonal_line += puzzle_grid[j][i];
-					diagonal_line.erase(find_if(diagonal_line.begin(), diagonal_line.end(), ::isspace), diagonal_line.end());
-					if (s.find(diagonal_line) != s.end() && (diagonal_line.length() >= 3))
-					{
-						v.push_back(diagonal_line);
+				if (SPIN < 8) {
+					int j = puzzle_grid.numrows() - SPIN + i;
+					if (j >= 0 && j < 8) {
+						diagonal_line += puzzle_grid[j][i];
+						diagonal_line.erase(find_if(diagonal_line.begin(), diagonal_line.end(), ::isspace), diagonal_line.end());
+						if (s.find(diagonal_line) != s.end() && (diagonal_line.length() >= 3))
+						{
+							v.insert(diagonal_line);
+						}
+					}
+				}
+			}
+		}
+	}
+	for (int a =0; a<6; a++){
+		for (int SPIN = 0; SPIN <= 8; SPIN++) {    // 좌측 상단에서 우측 하단으로 오른쪽 위에서부터 8회전 까지 (역방향)
+			string diagonal_line = "";
+			for (int i = SPIN - a; i >= 0; i--) {
+
+				if (SPIN <= 8) {
+					int j = puzzle_grid.numrows() - SPIN + i;
+					if (j >= 0 && j < 8) {
+						diagonal_line += puzzle_grid[i][j];
+						diagonal_line.erase(find_if(diagonal_line.begin(), diagonal_line.end(), ::isspace), diagonal_line.end());
+						if (s.find(diagonal_line) != s.end() && (diagonal_line.length() >= 3))
+						{
+							v.insert(diagonal_line);
+						}
+					}
+				}
+			}
+		}
+	}
+	for (int a= 0; a<6; a++){
+		for (int SPIN = 0; SPIN < 8; SPIN++) {      //좌측 상단에서 우측 하단으로 왼쪽 밑에서부터 8회전까지 (역방향)
+
+			string diagonal_line = "";
+			for (int i = SPIN - a; i >= 0; i--) {
+
+				if (SPIN < 8) {
+					int j = puzzle_grid.numrows() - SPIN + i;
+					if (j >= 0 && j < 8) {
+						diagonal_line += puzzle_grid[j][i];
+						diagonal_line.erase(find_if(diagonal_line.begin(), diagonal_line.end(), ::isspace), diagonal_line.end());
+						if (s.find(diagonal_line) != s.end() && (diagonal_line.length() >= 3))
+						{
+							v.insert(diagonal_line);
+						}
 					}
 				}
 			}
@@ -220,7 +231,11 @@ void make_word_in_puzzle(matrix<string> puzzle_grid) {
 
 vector<string> your_answer1()
 {
-	return v;
+	vector <string> end;
+	for (auto a : v) {
+		end.push_back(a);
+	}
+	return end;
 }
 
 // 여기까지 필요한 코드를 추가하세요
@@ -257,8 +272,6 @@ int main()
 	cout << "\n\n책목록을 프린트한다." << endl << endl;
 	booklist.print_booklist();
 
-
-
 	//------------------------------------------------------------//
 	// 여기서부터 필요한 코드를 추가하세요.
 	
@@ -268,21 +281,22 @@ int main()
 
 	puzzle.read_dic_file(puzzle_file);
 	make_word_in_puzzle(puzzle_grid);
-
+	
+	vector<string> words = your_answer1();
 	/*for (auto b : s) {
-		cout << "key : " << b << endl;
+		cout << b << " ";
 	}*/
 
 	/*for (auto b : v) {
 		cout << b << endl;
+		
 	}*/
-
-	vector<string> words = your_answer1();
-
+	
 	// 여기까지 필요한 코드를 추가하세요
 	//------------------------------------------------------------//
 
 	cout << "\n\n여러분의 정답을 출력합니다. " << endl << endl;
+	
 	print_result(words);
 	
 
